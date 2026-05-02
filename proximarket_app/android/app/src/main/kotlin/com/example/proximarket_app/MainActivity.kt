abf87742357c7a2d0ac6fda0a2eb29c1bcb71684
NAMILOHO <cnamil251@gmail.com>
2026-05-02 00:09:45 +0000

    Initial project setup

---
+++ proximarket_app/android/app/src/main/kotlin/com/example/proximarket_app/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.proximarket_app
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity : FlutterActivity()
---
+++ proximarket_app/lib/main.dart
@@ -0,0 +1,34 @@
+import 'package:flutter/material.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'firebase_options.dart';
+
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'ProxiMarket',
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(
+          seedColor: const Color(0xFF1D9E75),
+        ),
+        useMaterial3: true,
+      ),
+      home: const Scaffold(
+        body: Center(
+          child: Text('ProxiMarket 🚀'),
+        ),
+      ),
+    );
+  }
+}
